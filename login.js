59dc70e2849d7d7b539188433c5c5a2baa06d833
Claudio <102596993+clabiemann@users.noreply.github.com>
2022-09-12 19:33:50 -0300

    Add files via upload

---
+++ login.js
@@ -0,0 +1,20 @@
+
+var attempt = 3; // Variable de numeros de intentos.
+function validate(){
+var username = document.getElementById("username").value;
+var password = document.getElementById("password").value;
+if ( username == "Claudio" && password == "1280"){
+window.location = "edit.html"
+return false;
+}
+else{
+attempt --;// decrementa los intentos
+alert(" Error, quedan  "+attempt+" intentos ");
+if( attempt == 0){
+document.getElementById("username").disabled = true;
+document.getElementById("password").disabled = true;
+document.getElementById("submit").disabled = true;
+return false;
+}
+}
+}
